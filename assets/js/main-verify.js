b3b06bed3cafbc6a81f03db2d1fb11c7e08fddd1
zahed-zibaee <zahed.zibaee2@gmail.com>
2020-08-17 19:59:12 +0430

    change on verify

--- assets/js/main-verify.js
+++ assets/js/main-verify.js
@@ -1,7 +1,11 @@
 //verify informations
 $urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results[1] || 0;
+    if (results != null){
+        return results[1] || 0;
+    }else {
+        return null;
+    }
 }
 $("#name").text(decodeURIComponent($urlParam('name')));
 $("#family").text(decodeURIComponent($urlParam('family')));
@@ -11,9 +15,9 @@ if (decodeURIComponent($urlParam('gender')) == "M" ) {
     $("#gender").text("زن");
 }
 $("#father_name").text(decodeURIComponent($urlParam('father_name')));
-$("#code_meli").text(decodeURIComponent($urlParam('code_meli')));
-$("#phone").text(decodeURIComponent($urlParam('phone')));
-$("#address").text(decodeURIComponent($urlParam('address')));
+$("#code_meli").text($.persianNumbers(decodeURIComponent($urlParam('code_meli'))));
+$("#phone").text($.persianNumbers(decodeURIComponent($urlParam('phone'))));
+$("#address").text($.persianNumbers(decodeURIComponent($urlParam('address'))));
 if (decodeURIComponent($urlParam('class_time')) == "912" ) {
     $("#class_time").text("کلاس فشرده شنبه تا چهارشنبه ساعت ۹ تا ۱۲");
 }else if(decodeURIComponent($urlParam('class_time')) == "1316" ) {
@@ -29,9 +33,110 @@ if (decodeURIComponent($urlParam('payment_type')) == "option1" ) {
     $("#payment_type").text("اقساط");
 }
 
-$(document).on('click', '#back', function(e) {
-    e.preventDefault();
-    setTimeout(function() {
-        window.location.href = "./index.html?loading=off"
-    }, 1000)
+//
+// Mobile Verification
+//
+$(function() {
+    'use strict';
+
+    var body = $('#wrapper');
+    var flag_last = false;
+
+    
+    function goToPreviousInput(e){
+        var t = $(e.target),
+        sib = t.prev('input');
+
+        t[0].value = "";
+        sib.select().focus();    
+    }
+
+    function goToNextInput(e) {
+        var key = e.which,
+        t = $(e.target),
+        sib = t.next('input');
+
+        
+        if (t.hasClass("last")){
+            flag_last = true;
+        }else{
+            flag_last = false;
+        }
+        if ((key < 48 || (key > 57 && key < 96) || key > 105 )) {
+            e.preventDefault();
+            return false;
+        }
+        if (key === 9) {
+            return true;
+        }
+        
+        setTimeout(() => {
+            if (t[0].value.length > 1){
+                t[0].value = t[0].value[0];
+            }
+            if (flag_last === true) {
+                return false;
+            }
+            if (t[0].value.length !== 0){
+                sib.select().focus();
+            }
+            
+        }, 50);  
+    }
+    function onKeyDown(e) {
+        var key = e.which;
+        if (key === 8){
+            goToPreviousInput(e);
+        }
+        if ( (key === 9 || (key >= 48 && key <= 57 ) || (key >= 96 && key <= 105 )) && (flag_last === false || $(e.target)[0].value.length === 0) ) {
+            return true;
+        }
+        e.preventDefault();
+        return false;
+    }
+    function onFocus(e) {
+        $(e.target).select();
+    }
+
+    body.on('keyup', 'input', goToNextInput);
+    body.on('keydown', 'input', onKeyDown);
+    body.on('click', 'input', onFocus);
+
 });
+
+
+
+$(document).on('click', '#verify', function(e) {
+    e.preventDefault();
+    var ip = "";
+    $.ajax({
+        url: "https://api.ipify.org",
+        method: 'GET',
+        crossDomain: true,
+        success: function(res) {
+            ip = res;
+        },
+        error: function(e, v) {
+            alert("آدرس ip شما مشخص نیست");
+        }
+    });
+    var phone  = $urlParam('phone');
+    var url = "https://academyfarda.com/SMS/lookup"
+    $.ajax({
+        url: url,
+        method: 'POST',
+        data: phone,
+        crossDomain: true,
+        success: function(res) {
+          alert("sms sent");
+        },
+        error: function(e, v) {
+          dwtoast($form.find('.error-message').html())
+        }
+    });
+
+    setTimeout(() => {
+        $('#num1')[0].focus();
+    }, 500)
+});
+    
